e82061401fb36d1862af42246e4abd4c1f4fd68b
Riitta Pessi <riitta@pessi.fi>
2025-04-08 22:56:48 +0300

    Choose to live with cookie overflow instead of having even bigger problems

--- app/services/beermapping_api.rb
+++ app/services/beermapping_api.rb
@@ -8,14 +8,18 @@ class BeermappingApi
   def self.get_places_in(city)
     url = "https://studies.cs.helsinki.fi/beermapping/locations/"
 
-    response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
-    places = response.parsed_response["bmp_locations"]["location"]
+    begin
+      response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+      places = response.parsed_response["bmp_locations"]["location"]
+      return [] if places.is_a?(Hash) && places['id'].nil?
 
-    return [] if places.is_a?(Hash) && places['id'].nil?
-
-    places = [places] if places.is_a?(Hash)
-    places.map do |place|
-      Place.new(place)
+      places = [places] if places.is_a?(Hash)
+      places.map do |place|
+        Place.new(place)
+      end
+    rescue HTTParty::Error => e
+      Rails.logger.error "HTTParty error: #{e.message}"
+      []
     end
   end
 end
