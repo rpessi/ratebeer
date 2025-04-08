b358e8d4824a015e9bc254074c8598ff67b85eee
Riitta Pessi <riitta@pessi.fi>
2025-04-08 16:19:24 +0300

    Add some tests for beers

--- spec/features/beers_page_spec.rb
+++ spec/features/beers_page_spec.rb
@@ -11,30 +11,6 @@ describe "Beers page" do
       @user = FactoryBot.create :user
     end
 
-    it "allows a beer with a name to be added" do
-      sign_in(username: "Pekka", password: "Foobar1")
-      visit new_beer_path
-      fill_in('beer_name', with: 'Tasty Lager' )
-      select(@style.name)
-      select(@beer.brewery.name)
-      expect{
-        click_button('Create Beer')
-      }.to change{Beer.count}.by(1)
-      expect(page).to have_content "Beer was successfully created."
-    end
-
-    it "will not allow a beer without a name to be added" do
-      sign_in(username: "Pekka", password: "Foobar1")
-      visit new_beer_path
-      fill_in('beer_name', with: '' )
-      select(@style.name)
-      select(@beer.brewery.name)
-      expect{
-        click_button('Create Beer')
-      }.to change{Beer.count}.by(0)
-      expect(page).to have_content "Name can't be blank"
-    end
-
     it "shows the rating average for a beer" do
       FactoryBot.create(:rating, beer: @beer, score: 10, user: @user)
       visit beer_path(@beer)
@@ -43,6 +19,56 @@ describe "Beers page" do
       FactoryBot.create(:rating, beer: @beer, score: 15, user: @user)
       visit beer_path(@beer)
       expect(page).to have_content "Beer has 2 ratings with an average of 12.5"
+    end
+
+    describe "for a signed in user" do
+      before :each do
+        sign_in(username: "Pekka", password: "Foobar1")
+      end
+
+      it "allows a beer with a name to be added" do
+        sign_in(username: "Pekka", password: "Foobar1")
+        visit new_beer_path
+        fill_in('beer_name', with: 'Tasty Lager' )
+        select(@style.name)
+        select(@beer.brewery.name)
+        expect{
+          click_button('Create Beer')
+        }.to change{Beer.count}.by(1)
+        expect(page).to have_content "Beer was successfully created."
+      end
+  
+      it "will not allow a beer without a name to be added" do
+        sign_in(username: "Pekka", password: "Foobar1")
+        visit new_beer_path
+        fill_in('beer_name', with: '' )
+        select(@style.name)
+        select(@beer.brewery.name)
+        expect{
+          click_button('Create Beer')
+        }.to change{Beer.count}.by(0)
+        expect(page).to have_content "Name can't be blank"
+      end
+
+      it "will allow a beer to be updated" do
+        visit beer_path(@beer)
+        click_link "Edit this beer"
+        expect(page).to have_content "Editing beer"
+        fill_in('beer[name]', with: 'Better Factory Beer' )
+        click_button('Update Beer')
+        expect(Beer.count).to eq(1)
+        expect(page).to have_content "Beer was successfully updated."
+        expect(page).to have_content "Better Factory Beer"
+      end
+
+      it "will allow a beer to be destroyed" do
+        visit beer_path(@beer)
+        expect{
+          click_button "Destroy this beer"
+        }.to change{Beer.count}.by(-1)
+        expect(page).to have_content "Beer was successfully destroyed."
+      end
+      
     end
   end
 end
