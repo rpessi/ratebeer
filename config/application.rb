c468edc33541dec777c5c1c6d77686fdc3759387
Riitta Pessi <riitta@pessi.fi>
2025-04-08 19:34:18 +0300

    Fix cookieoverflow

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Ratebeer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
+    config.session_store :cache_store
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
